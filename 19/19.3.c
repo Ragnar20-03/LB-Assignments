bbd17409cea8153a848dff4e9b199444fac82912
Ragnar <roshanpp20@gmail.com>
2023-05-18 14:49:15 +0530

    19.1 , 2 3 .c

---
+++ 19/19.3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void Display(int iRow , int iCol)
+{
+    int iCnt = 0;
+    int jCnt = 0;
+    
+    for(iCnt = 1 ; iCnt <=iRow; iCnt++)
+    {
+        for (jCnt = 1; jCnt <= iCol; jCnt++)
+        {
+            if(iCnt == jCnt)
+            {
+                printf("$\t");
+            }
+            else if(iCnt < jCnt)
+            {
+                printf("#\t");
+            }
+            else{
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of Rows And Coloums .. \n");
+    scanf("%d%d",&iValue1 , &iValue2);
+
+    Display(iValue1 , iValue2);
+
+    return 0;
+}
