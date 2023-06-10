985a61bca71202d89781c41a9130dcb58afafc5b
Ragnar20-03 <roshanpp20@gmail.com>
2023-06-10 21:19:34 +0530

    28.5.cpp

---
+++ 28/28.5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+typedef unsigned int UINT;
+
+UINT ToggleNibble(UINT iNo)
+{
+    UINT iMask = 0X00000009;
+
+    return (iMask ^ iNo) ;
+    
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    cout<<"Enter the number : \n";
+    cin >> iValue;
+
+    iRet = ToggleNibble(iValue);
+
+    cout<<"Resulr after Toggle is : " <<iRet;
+
+    return 0;
+}
